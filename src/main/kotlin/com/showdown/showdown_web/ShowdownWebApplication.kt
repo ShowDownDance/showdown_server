564da8c0ad7edf4692d127502008ecd1ee05a1b7
davidolleh <asherolleh@gmail.com>
2024-03-11 12:12:14 +0900

    [feat] showdown web init

---
+++ src/main/kotlin/com/showdown/showdown_web/ShowdownWebApplication.kt
@@ -0,0 +1,11 @@
+package com.showdown.showdown_web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShowdownWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShowdownWebApplication>(*args)
+}
---
+++ src/test/kotlin/com/showdown/showdown_web/ShowdownWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.showdown.showdown_web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShowdownWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
