9e173b1797f58b866a157613d098779b8571f74d
davidolleh <asherolleh@gmail.com>
2024-03-17 21:45:00 +0900

    [feat] add scheduling annotation 앱이 스케줄러 사용할 수 있도록 설정정

--- src/main/kotlin/com/showdown/showdown_web/ShowdownWebApplication.kt
+++ src/main/kotlin/com/showdown/showdown_web/ShowdownWebApplication.kt
@@ -2,8 +2,10 @@ package com.showdown.showdown_web
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ShowdownWebApplication
 
 fun main(args: Array<String>) {
