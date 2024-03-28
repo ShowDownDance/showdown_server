1db0504bce2bb66e8407c8bb6578f6da21a03962
davidolleh <asherolleh@gmail.com>
2024-03-28 16:01:08 +0900

    [feat] 특정 조건에 맞는 lesson의 id를 구하는 query 추가

--- src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
+++ src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
@@ -3,5 +3,5 @@ package com.showdown.showdown_web.repository
 import com.showdown.showdown_web.entity.Lesson
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LessonRepository: JpaRepository<Lesson, Long> {
+interface LessonRepository: JpaRepository<Lesson, Long>, LessonRepositoryCustom {
 }
