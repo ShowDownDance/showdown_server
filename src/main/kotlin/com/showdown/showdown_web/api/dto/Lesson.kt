2efb100a79daa08ff0176a42f7d9957da9c088d5
davidolleh <asherolleh@gmail.com>
2024-04-01 16:08:35 +0900

    [feat] lesson api response dto 들 추가

---
+++ src/main/kotlin/com/showdown/showdown_web/api/dto/Lesson.kt
@@ -0,0 +1,56 @@
+package com.showdown.showdown_web.api.dto
+
+import com.showdown.showdown_web.entity.Lesson
+import java.time.format.DateTimeFormatter
+
+
+sealed class LessonResponse() {
+    data class DateAcademyLessonsDto(
+        val date: String,
+        val data: List<AcademyLessonsDto>
+    ): LessonResponse()
+
+    data class AcademyLessonsDto(
+        val academy: String,
+        val lessons: MutableList<LessonDto>
+    ): LessonResponse()
+
+    data class AcademyDateLessonsDto(
+        val academy: String,
+        val data: List<DateLessonsDto>
+    ): LessonResponse()
+
+    data class DateLessonsDto(
+        val date: String,
+        val lessons: MutableList<LessonDto>
+    ): LessonResponse()
+}
+
+
+
+data class LessonDto(
+    val startTime: String,
+    val endTime: String,
+    val dancer: String,
+    val level: String
+) {
+
+    companion object {
+        fun fromEntity(lesson: Lesson): LessonDto {
+            var dancer: String = ""
+            val dancersCount = lesson.lessonDancers.size
+            for ((index, d) in lesson.lessonDancers.withIndex()) {
+                dancer += d.dancer.name
+                if (index != dancersCount - 1)
+                    dancer += ", "
+            }
+
+            return LessonDto(
+                startTime = lesson.startTime.format(DateTimeFormatter.ofPattern("H:mm")),
+                endTime = lesson.endTime.format(DateTimeFormatter.ofPattern("H:mm")),
+                dancer = dancer,
+                level = lesson.level
+            )
+        }
+    }
+}
