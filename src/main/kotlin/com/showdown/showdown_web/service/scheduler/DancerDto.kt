68bddc855e3c5e3fd56b08e3691e461fc55b483b
davidolleh <asherolleh@gmail.com>
2024-04-01 14:32:31 +0900

    [fix] academy entity 패키지 추가

--- src/main/kotlin/com/showdown/showdown_web/service/scheduler/DancerDto.kt
+++ src/main/kotlin/com/showdown/showdown_web/service/scheduler/DancerDto.kt
@@ -1,4 +1,4 @@
-package com.showdown.showdown_web.service
+package com.showdown.showdown_web.service.scheduler
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.showdown.showdown_web.entity.Dancer
