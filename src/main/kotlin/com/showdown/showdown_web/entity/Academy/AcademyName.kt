0e0fcef4a2f05a1b91a21f00308e14b9477cf598
davidolleh <asherolleh@gmail.com>
2024-04-01 16:25:51 +0900

    [feat] string 값을 enum 값으로 바꾸는 함수 추가

--- src/main/kotlin/com/showdown/showdown_web/entity/Academy/AcademyName.kt
+++ src/main/kotlin/com/showdown/showdown_web/entity/Academy/AcademyName.kt
@@ -1,5 +1,16 @@
 package com.showdown.showdown_web.entity.Academy
 
 enum class AcademyName {
-    OneMillion, YGX, OFD
+    OneMillion, YGX, OFD;
+
+    companion object {
+        fun convertStringToAcademyName(name: String?): AcademyName? {
+            return when (name) {
+                OneMillion.name -> OneMillion
+                YGX.name -> YGX
+                OFD.name -> OFD
+                else -> null
+            }
+        }
+    }
 }
