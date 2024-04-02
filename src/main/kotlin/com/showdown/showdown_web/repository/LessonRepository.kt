ea4f026957d209026e1ddc4f8a57a6f4866106a4
davidolleh <asherolleh@gmail.com>
2024-04-02 15:25:47 +0900

    [fix] native query로 n+1문제 해결

--- src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
+++ src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
@@ -7,11 +7,16 @@ import org.springframework.data.jpa.repository.Query
 import java.time.LocalDate
 
 interface LessonRepository: JpaRepository<Lesson, Long>, LessonRepositoryCustom {
+
+    @Query(
+        "select l from Lesson l join fetch l.lessonDancers ld join fetch ld.dancer d " +
+                "where l.date = :date order by l.startTime"
+    )
     fun findLessonByDateOrderByStartTime(date: LocalDate) : List<Lesson>
 
 
     @Query("select l from Lesson l " +
-            "join fetch l.lessonDancers ld join fetch ld.dancer " +
+            "join fetch l.lessonDancers ld join fetch ld.dancer d " +
             "where l.date between :startDate and :lastDate order by l.date, l.startTime"
     )
     fun findLessonsByDateBetweenOrderByDate(startDate: LocalDate, lastDate: LocalDate) : List<Lesson>
