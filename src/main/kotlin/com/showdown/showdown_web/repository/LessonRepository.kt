210076ba0cb41423ccbc2bcfca53e45376def206
davidolleh <asherolleh@gmail.com>
2024-04-02 15:18:18 +0900

    [fix] n+1문제 해결

--- src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
+++ src/main/kotlin/com/showdown/showdown_web/repository/LessonRepository.kt
@@ -1,7 +1,18 @@
 package com.showdown.showdown_web.repository
 
+import com.showdown.showdown_web.entity.Academy.Academy
 import com.showdown.showdown_web.entity.Lesson
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDate
 
 interface LessonRepository: JpaRepository<Lesson, Long>, LessonRepositoryCustom {
+    fun findLessonByDateOrderByStartTime(date: LocalDate) : List<Lesson>
+
+
+    @Query("select l from Lesson l " +
+            "join fetch l.lessonDancers ld join fetch ld.dancer " +
+            "where l.date between :startDate and :lastDate order by l.date, l.startTime"
+    )
+    fun findLessonsByDateBetweenOrderByDate(startDate: LocalDate, lastDate: LocalDate) : List<Lesson>
 }
