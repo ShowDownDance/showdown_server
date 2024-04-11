a2aa38a059a5121428e5c22c17625bdbec2ed266
davidolleh <asherolleh@gmail.com>
2024-04-11 16:17:49 +0900

    <Feat> todo 추가 및 collection entity로 변경하는 함수 추가

--- src/main/kotlin/com/showdown/showdown_web/service/scheduler/DancerDto.kt
+++ src/main/kotlin/com/showdown/showdown_web/service/scheduler/DancerDto.kt
@@ -15,8 +15,13 @@ data class DancerDto(
             name = dancer.name,
             crew = dancer.crew
         )
+
+        //TODO:: 이게 여기 들어가는게 맞는지 고민해보기 새로운 객체를 하나 파야되는지
+        fun toEntities(dancers: Set<DancerDto>): Set<Dancer> = dancers.map {
+            it.toEntity()
+        }.toSet()
+
     }
 
     fun toEntity(): Dancer = Dancer(name = this.name, crew = this.crew)
-
 }
