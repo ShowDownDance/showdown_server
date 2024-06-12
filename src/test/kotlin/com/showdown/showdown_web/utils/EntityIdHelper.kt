b6052fa8bca7f44553fd396a1b40cc72973d0734
davidolleh <asherolleh@gmail.com>
2024-06-12 20:21:49 +0900

    <fix> 상속으로 인해 id annotaiton 찾지 못하던 부모 class에서 id annotaiton 찾도록 설정

--- src/test/kotlin/com/showdown/showdown_web/utils/EntityIdHelper.kt
+++ src/test/kotlin/com/showdown/showdown_web/utils/EntityIdHelper.kt
@@ -3,12 +3,19 @@ package com.showdown.showdown_web.utils
 import com.showdown.showdown_web.entity.common.BaseClass
 import jakarta.persistence.Id
 import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.superclasses
 import kotlin.reflect.jvm.javaField
 
 fun <T: BaseClass, U> setEntityId(entityId: U, entity: T): T {
     val kClass = entity.javaClass.kotlin
-    val idField = kClass.declaredMemberProperties.find { it.javaField?.getAnnotation(Id::class.java) != null }
-        ?: throw IllegalStateException("주어진 엔티티에 @Id 필드가 존재하지 않습니다")
+
+    val idSuperClass = kClass.superclasses.find {
+        it.simpleName.equals("BaseClass")
+    }   ?: throw IllegalStateException("주어진 엔티티에 @Id 필드가 존재하지 않습니다")
+
+    val idField = idSuperClass.declaredMemberProperties.find {
+        it.javaField?.getAnnotation(Id::class.java) != null
+    } ?: throw IllegalStateException("주어진 엔티티에 @Id 필드가 존재하지 않습니다")
     idField.javaField?.trySetAccessible()
     idField.javaField?.set(entity, entityId)
     return entity
