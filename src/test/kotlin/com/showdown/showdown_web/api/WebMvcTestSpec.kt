aaae32ae0bd917a377bf0537abaac2b854166c6c
davidolleh <asherolleh@gmail.com>
2024-06-12 20:23:05 +0900

    <feat> mockMVC로 테스트시 restdocs 적용하도록 설정

--- src/test/kotlin/com/showdown/showdown_web/api/WebMvcTestSpec.kt
+++ src/test/kotlin/com/showdown/showdown_web/api/WebMvcTestSpec.kt
@@ -2,20 +2,47 @@ package com.showdown.showdown_web.api
 
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.test.TestCase
 import io.kotest.extensions.spring.SpringExtension
-import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 
 
-//@ExtendWith(RestDocumentationExtension::class)
-//@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension::class)
+@AutoConfigureRestDocs
 @AutoConfigureWebMvc
 @AutoConfigureMockMvc
 @WebAppConfiguration
 //@Suppress("SpringJavaAutowiredMembersInspection")
 open class WebMvcTestSpec(body: FreeSpec.() -> Unit = {}) : FreeSpec(body) {
     override fun extensions(): List<Extension>  = listOf(SpringExtension)
+
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    @Autowired
+    private lateinit var restDocumentationContextProvider: RestDocumentationContextProvider
+
+    protected lateinit var mockMvc: MockMvc
+
+
+
+    override suspend fun beforeTest(testCase: TestCase) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply<DefaultMockMvcBuilder>(MockMvcRestDocumentation.documentationConfiguration(restDocumentationContextProvider))
+            .build()
+
+    }
 }
