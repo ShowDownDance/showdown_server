e4039af2830acb2a4b0ea1a87910168c71432036
davidolleh <asherolleh@gmail.com>
2024-06-15 21:28:13 +0900

    <fix> rest docs responseFields 중복 제거

---
+++ src/test/kotlin/com/showdown/showdown_web/api/utils/restdocs/ResponseBody.kt
@@ -0,0 +1,11 @@
+package com.showdown.showdown_web.api.utils.restdocs
+
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+
+fun responseBody(vararg fields: Field) = responseFields(fields.asList().map { it.type })
+
+fun responseBody(beneathPath: BeneathPath, vararg fields: Field) =
+    responseFields(
+        beneathPath.payloadSubsectionExtractor,
+        fields.asList().map { it.type }
+    )
